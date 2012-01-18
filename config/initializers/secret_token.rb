489112934fc3d284cd62d9fac1d0c35c414c0c1b
radcon <radcon.academy@gmail.com>
2012-01-18 16:33:04 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'b4f66be4a216e42b67d78f80fe73864c5539e2157cab392327191ff2e76adf5ce9d2d979626a370dceaf78f21386769aee55c2f44a6fced96ff4feafed380983'
